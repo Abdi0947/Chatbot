eb74af2dcf239ca1e4148d41189bab2ccc21fb45
abdi <debelaabdi3@gmail.com>
2024-10-18 22:32:50 +0300

    first commit

---
+++ my-app/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Chatbot from './components/chatbot'
+const App = () => {
+  return (
+    <div className='container'>
+      <Chatbot/>
+    </div>
+  )
+}
+
+export default App
---
+++ my-app/src/components/chatbot.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const chatbot = () => {
+  return (
+    <div className='start-page'>
+      <button className='start-page-btn'>Chat bot</button>
+    </div>
+  )
+}
+
+export default chatbot
